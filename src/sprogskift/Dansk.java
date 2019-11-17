d26a7aaff00bb152a30c61842cc75d646a3414c6
Thor <thor@10.14.6>
2019-11-17 19:47:38 +0100

    case added

--- src/sprogskift/Dansk.java
+++ src/sprogskift/Dansk.java
@@ -16,7 +16,9 @@ public class Dansk {
 		System.out.println("1) vis hundelisten");
 		System.out.println("2) vælg hund");
 		System.out.println("3) søg hund efter navn");
+		System.out.println("4) Skift til engelsk");
 		System.out.println("9) afslut programmet");
+		System.out.println("Indstast valg: ");
 	}
 	
 }
--- src/sprogskift/SprogSkift.java
+++ src/sprogskift/SprogSkift.java
@@ -1,22 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package sprogskift;
+
+import java.util.Scanner;
 
 /**
  *
  * @author thor
  */
 public class SprogSkift {
-
+	
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
+		int choice = 0;
+		Scanner sc = new Scanner(System.in);
 		Dansk sprog = new Dansk();
 		sprog.printMenu();
+		while (choice != 9) {
+			choice = sc.nextInt();
+			switch(choice) {
+				case 4:
+					changeLanguage();
+					break;
+				default:
+					choice = 9;
+					break;
+			}
+		}
 		// print en menu
 	}
 	
