56088d5a2523aacfe1b569c800e94fdeba742654
Thor <thor@10.14.6>
2019-11-17 19:37:52 +0100

    first

---
+++ src/sprogskift/Dansk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprogskift;
+
+/**
+ *
+ * @author thor
+ */
+public class Dansk {
+
+	public void printMenu() {
+		System.out.println("Velkommen til hundeuddelingen");
+		System.out.println("1) vis hundelisten");
+		System.out.println("2) vælg hund");
+		System.out.println("3) søg hund efter navn");
+		System.out.println("9) afslut programmet");
+	}
+	
+}
